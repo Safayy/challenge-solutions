bda2ba369b3f6c738d94f155df214d22a7d2d54d
Safa Yousif <safa.yousif@outlook.com>
2023-05-31 23:57:31 +0800

    Added daily

---
+++ codewars/python/8/5769b3802ae6f8e4890009d2.py
@@ -0,0 +1,9 @@
+def remove_every_other(my_list):
+    i = len(my_list) - 1
+    while i>=0:
+        if (i%2!=0):
+            my_list.pop(i)
+        i-=1
+    return my_list
+
+print( remove_every_other(["Keep", "Remove", "Keep", "Remove", "Keep"]) , end=" ")
