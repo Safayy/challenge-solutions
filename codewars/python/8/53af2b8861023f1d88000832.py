0dd7b5612f97f9de27c08bb9a0dd36abbbe638a2
Safa Yousif <safa.yousif@outlook.com>
2023-06-02 23:02:14 +0800

    Fix bug

--- codewars/python/8/53af2b8861023f1d88000832.py
+++ codewars/python/8/53af2b8861023f1d88000832.py
@@ -1,4 +1,4 @@
 def are_you_playing_banjo(name):
-    return f"{name} " + "plays banjo" if name[0].lower()=='r' else "doesnt play"
+    return f"{name} " + ("plays banjo" if name[0].lower()=='r' else "does not play banjo")
 
 print( are_you_playing_banjo("Rafa") )
